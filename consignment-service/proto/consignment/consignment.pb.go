a0a8c15a4843d397c2cb1f94840c93b85b9f5e09
root <root@localhost.localdomain>
2019-02-15 10:29:10 -0500

    ajust

--- consignment-service/proto/consignment/consignment.pb.go
+++ consignment-service/proto/consignment/consignment.pb.go
@@ -3,13 +3,12 @@
 
 package go_micro_srv_consignment
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
-
 import (
-	context "golang.org/x/net/context"
+	context "context"
+	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
 	grpc "google.golang.org/grpc"
+	math "math"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -21,15 +20,15 @@ var _ = math.Inf
 // is compatible with the proto package it is being compiled against.
 // A compilation error at this line likely means your copy of the
 // proto package needs to be updated.
-const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
+const _ = proto.ProtoPackageIsVersion3 // please upgrade the proto package
 
 // 货轮承运的一批货物
 type Consignment struct {
-	Id                   string       `protobuf:"bytes,1,opt,name=id" json:"id,omitempty"`
-	Description          string       `protobuf:"bytes,2,opt,name=description" json:"description,omitempty"`
-	Weight               int32        `protobuf:"varint,3,opt,name=weight" json:"weight,omitempty"`
-	Containers           []*Container `protobuf:"bytes,4,rep,name=containers" json:"containers,omitempty"`
-	VesselId             string       `protobuf:"bytes,5,opt,name=vessel_id,json=vesselId" json:"vessel_id,omitempty"`
+	Id                   string       `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Description          string       `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
+	Weight               int32        `protobuf:"varint,3,opt,name=weight,proto3" json:"weight,omitempty"`
+	Containers           []*Container `protobuf:"bytes,4,rep,name=containers,proto3" json:"containers,omitempty"`
+	VesselId             string       `protobuf:"bytes,5,opt,name=vessel_id,json=vesselId,proto3" json:"vessel_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}     `json:"-"`
 	XXX_unrecognized     []byte       `json:"-"`
 	XXX_sizecache        int32        `json:"-"`
@@ -39,16 +38,17 @@ func (m *Consignment) Reset()         { *m = Consignment{} }
 func (m *Consignment) String() string { return proto.CompactTextString(m) }
 func (*Consignment) ProtoMessage()    {}
 func (*Consignment) Descriptor() ([]byte, []int) {
-	return fileDescriptor_consignment_763716e9aaf5e417, []int{0}
+	return fileDescriptor_e5e5ab05dfa973d5, []int{0}
 }
+
 func (m *Consignment) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Consignment.Unmarshal(m, b)
 }
 func (m *Consignment) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Consignment.Marshal(b, m, deterministic)
 }
-func (dst *Consignment) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Consignment.Merge(dst, src)
+func (m *Consignment) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Consignment.Merge(m, src)
 }
 func (m *Consignment) XXX_Size() int {
 	return xxx_messageInfo_Consignment.Size(m)
@@ -96,10 +96,10 @@ func (m *Consignment) GetVesselId() string {
 
 // 单个集装箱
 type Container struct {
-	Id                   string   `protobuf:"bytes,1,opt,name=id" json:"id,omitempty"`
-	CustomerId           string   `protobuf:"bytes,2,opt,name=customer_id,json=customerId" json:"customer_id,omitempty"`
-	Origin               string   `protobuf:"bytes,3,opt,name=origin" json:"origin,omitempty"`
-	UserId               string   `protobuf:"bytes,4,opt,name=user_id,json=userId" json:"user_id,omitempty"`
+	Id                   string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	CustomerId           string   `protobuf:"bytes,2,opt,name=customer_id,json=customerId,proto3" json:"customer_id,omitempty"`
+	Origin               string   `protobuf:"bytes,3,opt,name=origin,proto3" json:"origin,omitempty"`
+	UserId               string   `protobuf:"bytes,4,opt,name=user_id,json=userId,proto3" json:"user_id,omitempty"`
 	XXX_NoUnkeyedLiteral struct{} `json:"-"`
 	XXX_unrecognized     []byte   `json:"-"`
 	XXX_sizecache        int32    `json:"-"`
@@ -109,16 +109,17 @@ func (m *Container) Reset()         { *m = Container{} }
 func (m *Container) String() string { return proto.CompactTextString(m) }
 func (*Container) ProtoMessage()    {}
 func (*Container) Descriptor() ([]byte, []int) {
-	return fileDescriptor_consignment_763716e9aaf5e417, []int{1}
+	return fileDescriptor_e5e5ab05dfa973d5, []int{1}
 }
+
 func (m *Container) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Container.Unmarshal(m, b)
 }
 func (m *Container) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Container.Marshal(b, m, deterministic)
 }
-func (dst *Container) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Container.Merge(dst, src)
+func (m *Container) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Container.Merge(m, src)
 }
 func (m *Container) XXX_Size() int {
 	return xxx_messageInfo_Container.Size(m)
@@ -159,9 +160,9 @@ func (m *Container) GetUserId() string {
 
 // 托运结果
 type Response struct {
-	Created              bool           `protobuf:"varint,1,opt,name=created" json:"created,omitempty"`
-	Consignment          *Consignment   `protobuf:"bytes,2,opt,name=consignment" json:"consignment,omitempty"`
-	Consignments         []*Consignment `protobuf:"bytes,3,rep,name=consignments" json:"consignments,omitempty"`
+	Created              bool           `protobuf:"varint,1,opt,name=created,proto3" json:"created,omitempty"`
+	Consignment          *Consignment   `protobuf:"bytes,2,opt,name=consignment,proto3" json:"consignment,omitempty"`
+	Consignments         []*Consignment `protobuf:"bytes,3,rep,name=consignments,proto3" json:"consignments,omitempty"`
 	XXX_NoUnkeyedLiteral struct{}       `json:"-"`
 	XXX_unrecognized     []byte         `json:"-"`
 	XXX_sizecache        int32          `json:"-"`
@@ -171,16 +172,17 @@ func (m *Response) Reset()         { *m = Response{} }
 func (m *Response) String() string { return proto.CompactTextString(m) }
 func (*Response) ProtoMessage()    {}
 func (*Response) Descriptor() ([]byte, []int) {
-	return fileDescriptor_consignment_763716e9aaf5e417, []int{2}
+	return fileDescriptor_e5e5ab05dfa973d5, []int{2}
 }
+
 func (m *Response) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Response.Unmarshal(m, b)
 }
 func (m *Response) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_Response.Marshal(b, m, deterministic)
 }
-func (dst *Response) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_Response.Merge(dst, src)
+func (m *Response) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_Response.Merge(m, src)
 }
 func (m *Response) XXX_Size() int {
 	return xxx_messageInfo_Response.Size(m)
@@ -224,16 +226,17 @@ func (m *GetRequest) Reset()         { *m = GetRequest{} }
 func (m *GetRequest) String() string { return proto.CompactTextString(m) }
 func (*GetRequest) ProtoMessage()    {}
 func (*GetRequest) Descriptor() ([]byte, []int) {
-	return fileDescriptor_consignment_763716e9aaf5e417, []int{3}
+	return fileDescriptor_e5e5ab05dfa973d5, []int{3}
 }
+
 func (m *GetRequest) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_GetRequest.Unmarshal(m, b)
 }
 func (m *GetRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
 	return xxx_messageInfo_GetRequest.Marshal(b, m, deterministic)
 }
-func (dst *GetRequest) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_GetRequest.Merge(dst, src)
+func (m *GetRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_GetRequest.Merge(m, src)
 }
 func (m *GetRequest) XXX_Size() int {
 	return xxx_messageInfo_GetRequest.Size(m)
@@ -251,6 +254,36 @@ func init() {
 	proto.RegisterType((*GetRequest)(nil), "go.micro.srv.consignment.GetRequest")
 }
 
+func init() {
+	proto.RegisterFile("proto/consignment/consignment.proto", fileDescriptor_e5e5ab05dfa973d5)
+}
+
+var fileDescriptor_e5e5ab05dfa973d5 = []byte{
+	// 351 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0x4f, 0x4e, 0xf3, 0x30,
+	0x10, 0xc5, 0xbf, 0xf4, 0x7f, 0x26, 0xd5, 0x57, 0xe1, 0x05, 0x58, 0xb0, 0x20, 0x4a, 0x41, 0xea,
+	0x2a, 0x48, 0xe5, 0x08, 0x59, 0x54, 0xd9, 0xba, 0x6b, 0x04, 0x25, 0x1e, 0xa5, 0x96, 0x88, 0x1d,
+	0x6c, 0xb7, 0x5c, 0x0d, 0xae, 0xc1, 0x89, 0x50, 0x9d, 0x86, 0x1a, 0xa1, 0xa2, 0xee, 0xfc, 0x66,
+	0xfc, 0xf3, 0x7b, 0x63, 0x0d, 0x4c, 0x6b, 0xad, 0xac, 0xba, 0x2b, 0x94, 0x34, 0xa2, 0x94, 0x15,
+	0x4a, 0xeb, 0x9f, 0x53, 0xd7, 0x25, 0xb4, 0x54, 0x69, 0x25, 0x0a, 0xad, 0x52, 0xa3, 0xb7, 0xa9,
+	0xd7, 0x4f, 0x3e, 0x02, 0x88, 0xb2, 0x83, 0x26, 0xff, 0xa1, 0x23, 0x38, 0x0d, 0xe2, 0x60, 0x16,
+	0xb2, 0x8e, 0xe0, 0x24, 0x86, 0x88, 0xa3, 0x29, 0xb4, 0xa8, 0xad, 0x50, 0x92, 0x76, 0x5c, 0xc3,
+	0x2f, 0x91, 0x73, 0x18, 0xbc, 0xa1, 0x28, 0xd7, 0x96, 0x76, 0xe3, 0x60, 0xd6, 0x67, 0x7b, 0x45,
+	0x32, 0x80, 0x42, 0x49, 0xbb, 0x12, 0x12, 0xb5, 0xa1, 0xbd, 0xb8, 0x3b, 0x8b, 0xe6, 0xd3, 0xf4,
+	0x58, 0x90, 0x34, 0x6b, 0xef, 0x32, 0x0f, 0x23, 0x57, 0x10, 0x6e, 0xd1, 0x18, 0x7c, 0x79, 0x14,
+	0x9c, 0xf6, 0x9d, 0xf9, 0xa8, 0x29, 0xe4, 0x3c, 0xa9, 0x20, 0xfc, 0xa6, 0x7e, 0x05, 0xbf, 0x86,
+	0xa8, 0xd8, 0x18, 0xab, 0x2a, 0xd4, 0x3b, 0xb6, 0x09, 0x0e, 0x6d, 0x29, 0xe7, 0xbb, 0xdc, 0x4a,
+	0x8b, 0x52, 0x48, 0x97, 0x3b, 0x64, 0x7b, 0x45, 0x2e, 0x60, 0xb8, 0x31, 0x0d, 0xd4, 0x6b, 0x1a,
+	0x3b, 0x99, 0xf3, 0xe4, 0x3d, 0x80, 0x11, 0x43, 0x53, 0x2b, 0x69, 0x90, 0x50, 0x18, 0x16, 0x1a,
+	0x57, 0x16, 0x1b, 0xcf, 0x11, 0x6b, 0x25, 0x59, 0x40, 0xe4, 0xcd, 0xe5, 0x8c, 0xa3, 0xf9, 0xed,
+	0x9f, 0x83, 0xb7, 0x67, 0xe6, 0x93, 0x24, 0x87, 0xb1, 0x27, 0x0d, 0xed, 0xba, 0x2f, 0x3c, 0xf1,
+	0xa5, 0x1f, 0x68, 0x32, 0x06, 0x58, 0xa0, 0x65, 0xf8, 0xba, 0x41, 0x63, 0xe7, 0x9f, 0x01, 0x4c,
+	0x96, 0x6b, 0x51, 0xd7, 0x42, 0x96, 0x4b, 0xd4, 0x5b, 0x51, 0x20, 0x79, 0x82, 0xb3, 0xcc, 0x0d,
+	0xe0, 0x2f, 0xc3, 0x69, 0x5e, 0x97, 0xc9, 0xf1, 0x6b, 0xed, 0x7f, 0x25, 0xff, 0xc8, 0x03, 0x4c,
+	0x16, 0x68, 0x3d, 0xce, 0x90, 0x9b, 0xe3, 0xe0, 0x21, 0xee, 0x69, 0xcf, 0x3f, 0x0f, 0xdc, 0xa6,
+	0xdf, 0x7f, 0x05, 0x00, 0x00, 0xff, 0xff, 0x0e, 0x16, 0x55, 0x5f, 0x10, 0x03, 0x00, 0x00,
+}
+
 // Reference imports to suppress errors if they are not otherwise used.
 var _ context.Context
 var _ grpc.ClientConn
@@ -259,8 +292,9 @@ var _ grpc.ClientConn
 // is compatible with the grpc package it is being compiled against.
 const _ = grpc.SupportPackageIsVersion4
 
-// Client API for ShippingService service
-
+// ShippingServiceClient is the client API for ShippingService service.
+//
+// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type ShippingServiceClient interface {
 	// 托运一批货物
 	CreateConsignment(ctx context.Context, in *Consignment, opts ...grpc.CallOption) (*Response, error)
@@ -278,7 +312,7 @@ func NewShippingServiceClient(cc *grpc.ClientConn) ShippingServiceClient {
 
 func (c *shippingServiceClient) CreateConsignment(ctx context.Context, in *Consignment, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := grpc.Invoke(ctx, "/go.micro.srv.consignment.ShippingService/CreateConsignment", in, out, c.cc, opts...)
+	err := c.cc.Invoke(ctx, "/go.micro.srv.consignment.ShippingService/CreateConsignment", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -287,15 +321,14 @@ func (c *shippingServiceClient) CreateConsignment(ctx context.Context, in *Consi
 
 func (c *shippingServiceClient) GetConsignments(ctx context.Context, in *GetRequest, opts ...grpc.CallOption) (*Response, error) {
 	out := new(Response)
-	err := grpc.Invoke(ctx, "/go.micro.srv.consignment.ShippingService/GetConsignments", in, out, c.cc, opts...)
+	err := c.cc.Invoke(ctx, "/go.micro.srv.consignment.ShippingService/GetConsignments", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// Server API for ShippingService service
-
+// ShippingServiceServer is the server API for ShippingService service.
 type ShippingServiceServer interface {
 	// 托运一批货物
 	CreateConsignment(context.Context, *Consignment) (*Response, error)
@@ -358,34 +391,4 @@ var _ShippingService_serviceDesc = grpc.ServiceDesc{
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "proto/consignment/consignment.proto",
-}
-
-func init() {
-	proto.RegisterFile("proto/consignment/consignment.proto", fileDescriptor_consignment_763716e9aaf5e417)
-}
-
-var fileDescriptor_consignment_763716e9aaf5e417 = []byte{
-	// 351 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0x4f, 0x4e, 0xf3, 0x30,
-	0x10, 0xc5, 0xbf, 0xf4, 0x7f, 0x26, 0xd5, 0x57, 0xe1, 0x05, 0x58, 0xb0, 0x20, 0x4a, 0x41, 0xea,
-	0x2a, 0x48, 0xe5, 0x08, 0x59, 0x54, 0xd9, 0xba, 0x6b, 0x04, 0x25, 0x1e, 0xa5, 0x96, 0x88, 0x1d,
-	0x6c, 0xb7, 0x5c, 0x0d, 0xae, 0xc1, 0x89, 0x50, 0x9d, 0x86, 0x1a, 0xa1, 0xa2, 0xee, 0xfc, 0x66,
-	0xfc, 0xf3, 0x7b, 0x63, 0x0d, 0x4c, 0x6b, 0xad, 0xac, 0xba, 0x2b, 0x94, 0x34, 0xa2, 0x94, 0x15,
-	0x4a, 0xeb, 0x9f, 0x53, 0xd7, 0x25, 0xb4, 0x54, 0x69, 0x25, 0x0a, 0xad, 0x52, 0xa3, 0xb7, 0xa9,
-	0xd7, 0x4f, 0x3e, 0x02, 0x88, 0xb2, 0x83, 0x26, 0xff, 0xa1, 0x23, 0x38, 0x0d, 0xe2, 0x60, 0x16,
-	0xb2, 0x8e, 0xe0, 0x24, 0x86, 0x88, 0xa3, 0x29, 0xb4, 0xa8, 0xad, 0x50, 0x92, 0x76, 0x5c, 0xc3,
-	0x2f, 0x91, 0x73, 0x18, 0xbc, 0xa1, 0x28, 0xd7, 0x96, 0x76, 0xe3, 0x60, 0xd6, 0x67, 0x7b, 0x45,
-	0x32, 0x80, 0x42, 0x49, 0xbb, 0x12, 0x12, 0xb5, 0xa1, 0xbd, 0xb8, 0x3b, 0x8b, 0xe6, 0xd3, 0xf4,
-	0x58, 0x90, 0x34, 0x6b, 0xef, 0x32, 0x0f, 0x23, 0x57, 0x10, 0x6e, 0xd1, 0x18, 0x7c, 0x79, 0x14,
-	0x9c, 0xf6, 0x9d, 0xf9, 0xa8, 0x29, 0xe4, 0x3c, 0xa9, 0x20, 0xfc, 0xa6, 0x7e, 0x05, 0xbf, 0x86,
-	0xa8, 0xd8, 0x18, 0xab, 0x2a, 0xd4, 0x3b, 0xb6, 0x09, 0x0e, 0x6d, 0x29, 0xe7, 0xbb, 0xdc, 0x4a,
-	0x8b, 0x52, 0x48, 0x97, 0x3b, 0x64, 0x7b, 0x45, 0x2e, 0x60, 0xb8, 0x31, 0x0d, 0xd4, 0x6b, 0x1a,
-	0x3b, 0x99, 0xf3, 0xe4, 0x3d, 0x80, 0x11, 0x43, 0x53, 0x2b, 0x69, 0x90, 0x50, 0x18, 0x16, 0x1a,
-	0x57, 0x16, 0x1b, 0xcf, 0x11, 0x6b, 0x25, 0x59, 0x40, 0xe4, 0xcd, 0xe5, 0x8c, 0xa3, 0xf9, 0xed,
-	0x9f, 0x83, 0xb7, 0x67, 0xe6, 0x93, 0x24, 0x87, 0xb1, 0x27, 0x0d, 0xed, 0xba, 0x2f, 0x3c, 0xf1,
-	0xa5, 0x1f, 0x68, 0x32, 0x06, 0x58, 0xa0, 0x65, 0xf8, 0xba, 0x41, 0x63, 0xe7, 0x9f, 0x01, 0x4c,
-	0x96, 0x6b, 0x51, 0xd7, 0x42, 0x96, 0x4b, 0xd4, 0x5b, 0x51, 0x20, 0x79, 0x82, 0xb3, 0xcc, 0x0d,
-	0xe0, 0x2f, 0xc3, 0x69, 0x5e, 0x97, 0xc9, 0xf1, 0x6b, 0xed, 0x7f, 0x25, 0xff, 0xc8, 0x03, 0x4c,
-	0x16, 0x68, 0x3d, 0xce, 0x90, 0x9b, 0xe3, 0xe0, 0x21, 0xee, 0x69, 0xcf, 0x3f, 0x0f, 0xdc, 0xa6,
-	0xdf, 0x7f, 0x05, 0x00, 0x00, 0xff, 0xff, 0x0e, 0x16, 0x55, 0x5f, 0x10, 0x03, 0x00, 0x00,
 }
